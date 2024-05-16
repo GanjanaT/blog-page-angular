1c602ce1c03a799503cb6cdaee35bafe0520e768
Ganjana <ganjana.thinpho@utb.ecutbildning.se>
2024-05-16 12:51:21 +0200

    sort blog posts, db.json change, took away scroll to bottom on home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { BlogFormComponent } from './components/blog-form/blog-form.component';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
@@ -17,23 +17,17 @@ import { CommonModule } from '@angular/common';
 export class HomePageComponent implements OnInit {
   blogPosts: BlogPost[] = [];
   blogPostsExists : boolean = true;
-
-  @ViewChild('blogContainer') blogContainer! : ElementRef;
   blogPostService: BlogPostService = inject(BlogPostService);
 
   ngOnInit(): void {
     this.loadBlogPosts();
-    this.blogPostService.reload.subscribe(() => {
-      this.loadBlogPosts(); 
-      setTimeout(() => {
-      this.scrollToBottom()
-    }, 200)});
+    this.blogPostService.reload.subscribe(() => this.loadBlogPosts());
   }
 
   loadBlogPosts() {
     this.blogPostService
       .getBlogPosts()
-      .subscribe((blogPosts) => this.blogPosts = blogPosts);
+      .subscribe((blogPosts) => this.blogPosts = blogPosts.sort((a,b) => +new Date(a.date) - +new Date(b.date)).reverse());
   }
 
   onFilterBlogPosts(blogPosts : BlogPost[]){
@@ -48,9 +42,5 @@ export class HomePageComponent implements OnInit {
   onReturn(){
     this.blogPostsExists = true;
     this.loadBlogPosts()
-  }
-
-  scrollToBottom(){
-    this.blogContainer.nativeElement.scrollTop = this.blogContainer.nativeElement.scrollHeight;
   }
 }
